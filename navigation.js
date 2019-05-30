63152fc150a053ebc33cd2a711c24806e098e7dd
mylove13289 <709834360@qq.com>
2019-05-30 09:30:59 +0800

    Add files via upload
    导航文件的模板

---
+++ navigation.js
@@ -0,0 +1,38 @@
+{
+	"commonSiteList":[
+
+		{"name":"CSND","icon":"CSND","url":"URL"},
+		{"name":"CSND","icon":"CSND","url":"URL"},
+		{"name":"CSND","icon":"CSND","url":"URL"},
+		{"name":"CSND","icon":"CSND","url":"URL"}
+
+	],
+
+	"categoryList":[
+
+		{
+			"categoryName":"xx","desc":"","categorySiteList":[
+
+				{"name":"CSND","icon":"CSND","url":"URL"},
+				{"name":"CSND","icon":"CSND","url":"URL"},
+				{"name":"CSND","icon":"CSND","url":"URL"},
+				{"name":"CSND","icon":"CSND","url":"URL"}
+
+			]
+		},
+
+		{
+			"categoryName":"yy","desc":"","categorySiteList":[
+
+				{"name":"CSND","icon":"CSND","url":"URL"},
+				{"name":"CSND","icon":"CSND","url":"URL"},
+				{"name":"CSND","icon":"CSND","url":"URL"},
+				{"name":"CSND","icon":"CSND","url":"URL"}
+
+			]
+		},
+
+
+	]
+
+}
