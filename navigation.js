c678a8cfc07edb6a69b0ed5f72c601a456ffe32e
longquan <longquan@dian.so>
2019-06-02 15:02:23 +0800

    提交新内容。

--- navigation.js
+++ navigation.js
@@ -46,9 +46,9 @@
         {
             "categoryName": "接私活", "desc": "", "categorySiteList": [
 
-                {"name": "CSND", "url": "https://www.cnblogs.com/", "icon": "URL"},
-                {"name": "CSND", "url": "http://www.csdn.net/", "icon": "URL"},
-                {"name": "CSND", "url": "http://www.jianshu.com/", "icon": "URL"}
+                {"name": "CSND", "url": "https://www.cnblogs.com/", "icon": ""},
+                {"name": "CSND", "url": "http://www.csdn.net/", "icon": ""},
+                {"name": "CSND", "url": "http://www.jianshu.com/", "icon": ""}
 
             ]
         },
@@ -57,9 +57,9 @@
         {
             "categoryName": "IT资讯", "desc": "", "categorySiteList": [
 
-                {"name": "CSND", "url": "http://36kr.com/", "icon": "URL"},
-                {"name": "CSND", "url": "https://www.huxiu.com/", "icon": "URL"},
-                {"name": "CSND", "url": "http://www.cnetnews.com.cn/", "icon": "URL"}
+                {"name": "CSND", "url": "http://36kr.com/", "icon": ""},
+                {"name": "CSND", "url": "https://www.huxiu.com/", "icon": ""},
+                {"name": "CSND", "url": "http://www.cnetnews.com.cn/", "icon": ""}
 
 
             ]
@@ -69,15 +69,15 @@
         {
             "categoryName": "看新闻", "desc": "", "categorySiteList": [
 
-                {"name": "网易新闻", "url": "http://news.163.com/", "icon": "URL"},
-                {"name": "凤凰资讯", "url": "http://news.ifeng.com/", "icon": "URL"},
-                {"name": "人民网", "url": "http://www.people.com.cn/", "icon": "URL"},
-                {"name": "南方都市报", "url": "http://www.nandu.com/", "icon": "URL"},
-                {"name": "美国CNN", "url": "http://www.cnn.com/", "icon": "URL"},
-                {"name": "联合国新闻", "url": "http://www.un.org/chinese/News/", "icon": "URL"},
-                {"name": "环球军事", "url": "http://mil.huanqiu.com/", "icon": "URL"},
-                {"name": "凤凰网军事", "url": "http://news.ifeng.com/mil/", "icon": "URL"},
-                {"name": "国防部", "url": "http://www.mod.gov.cn/", "icon": "URL"}
+                {"name": "网易新闻", "url": "http://news.163.com/", "icon": ""},
+                {"name": "凤凰资讯", "url": "http://news.ifeng.com/", "icon": ""},
+                {"name": "人民网", "url": "http://www.people.com.cn/", "icon": ""},
+                {"name": "南方都市报", "url": "http://www.nandu.com/", "icon": ""},
+                {"name": "美国CNN", "url": "http://www.cnn.com/", "icon": ""},
+                {"name": "联合国新闻", "url": "http://www.un.org/chinese/News/", "icon": ""},
+                {"name": "环球军事", "url": "http://mil.huanqiu.com/", "icon": ""},
+                {"name": "凤凰网军事", "url": "http://news.ifeng.com/mil/", "icon": ""},
+                {"name": "国防部", "url": "http://www.mod.gov.cn/", "icon": ""}
 
             ]
         },
@@ -85,9 +85,9 @@
         {
             "categoryName": "休闲一刻", "desc": "", "categorySiteList": [
 
-                {"name": "19楼", "url": "http://www.19lou.com/", "icon": "URL"},
-                {"name": "糗事百科", "url": "http://www.qiushibaike.com/", "icon": "URL"},
-                {"name": "暴走漫画", "url": "http://baozoumanhua.com/", "icon": "URL"}
+                {"name": "19楼", "url": "http://www.19lou.com/", "icon": ""},
+                {"name": "糗事百科", "url": "http://www.qiushibaike.com/", "icon": ""},
+                {"name": "暴走漫画", "url": "http://baozoumanhua.com/", "icon": ""}
 
             ]
         },
@@ -96,10 +96,10 @@
             "categoryName": "热爱美图", "desc": "", "categorySiteList": [
 
                 {"name": "妹子美图", "url": "http://www.meizitu.com/", "icon": ""},
-                {"name": "美女高清", "url": "http://www.qiushibaike.com/", "icon": "URL"},
-                {"name": "美女私房照", "url": "http://www.mnsfz.com/", "icon": "URL"},
-                {"name": "4493美图", "url": "http://www.4493.com/", "icon": "URL"},
-                {"name": "美女图片", "url": "http://www.169bb.com/", "icon": "URL"}
+                {"name": "美女高清", "url": "http://www.qiushibaike.com/", "icon": ""},
+                {"name": "美女私房照", "url": "http://www.mnsfz.com/", "icon": ""},
+                {"name": "4493美图", "url": "http://www.4493.com/", "icon": ""},
+                {"name": "美女图片", "url": "http://www.169bb.com/", "icon": ""}
 
             ]
         },
