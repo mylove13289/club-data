cd6ba813e12c178f202cb1abe3ea112c1237bae0
longquan <longquan@dian.so>
2019-06-02 09:14:54 +0800

    提交新内容。

--- navigation.js
+++ navigation.js
@@ -1,38 +1,112 @@
 {
-	"commonSiteList":[
+    "commonSiteList"
+:
+    [
 
-		{"name":"CSND","icon":"CSND","url":"URL"},
-		{"name":"CSND","icon":"CSND","url":"URL"},
-		{"name":"CSND","icon":"CSND","url":"URL"},
-		{"name":"CSND","icon":"CSND","url":"URL"}
+        {"name": "GOOGLE", "icon": "CSND", "url": "http://www.google.com"},
+        {"name": "百度", "icon": "https://www.baidu.com/favicon.ico", "url": "https://www.baidu.com/"},
+        {"name": "开源中国", "icon": "https://static.oschina.net/new-osc/img/favicon.ico", "url": "https://www.oschina.net/"},
+        {"name": "INFOQ", "icon": "https://static001.infoq.cn/static/infoq/www/img/InfoQ-share-icon2.jpg", "url": "https://www.infoq.cn/"},
+        {"name": "GITHUB", "icon": "https://github.com/fluidicon.png", "url": "https://github.com/"},
+        {"name": "并发网站", "icon": "", "url": "http://ifeve.com/"},
+        {"name": "StackOverFlow", "icon": "CSND", "url": "http://stackoverflow.com/"},
+        {
+            "name": "segmentfault",
+            "url": "https://segmentfault.com/",
+            "icon": "https://static.segmentfault.com/v-5cc2cd8e/global/img/touch-icon.png"
+        },
+        {"name": "知乎", "icon": "CSND", "url": "http://www.zhihu.com/"},
 
-	],
+        {"name": "CSND", "url": "http://www.csdn.net/", "icon": "https://csdnimg.cn/public/favicon.ico"},
+        {"name": "博客园", "url": "https://www.cnblogs.com/", "icon": "https://common.cnblogs.com/favicon.ico"},
+        {"name": "processON", "url": "https://www.processon.com/", "icon": "https://www.processon.com/favicon.ico"},
 
-	"categoryList":[
-
-		{
-			"categoryName":"xx","desc":"","categorySiteList":[
-
-				{"name":"CSND","icon":"CSND","url":"URL"},
-				{"name":"CSND","icon":"CSND","url":"URL"},
-				{"name":"CSND","icon":"CSND","url":"URL"},
-				{"name":"CSND","icon":"CSND","url":"URL"}
-
-			]
-		},
-
-		{
-			"categoryName":"yy","desc":"","categorySiteList":[
-
-				{"name":"CSND","icon":"CSND","url":"URL"},
-				{"name":"CSND","icon":"CSND","url":"URL"},
-				{"name":"CSND","icon":"CSND","url":"URL"},
-				{"name":"CSND","icon":"CSND","url":"URL"}
-
-			]
-		},
+        {"name": "码农民", "url": "http://www.codeceo.com/", "icon": "http://static.codeceo.com/favicon.ico"},
+        {"name": "快递查询", "url": "http://www.kuaidi.com/", "icon": ""}
 
 
-	]
+    ],
+
+        "categoryList"
+:
+    [
+
+        {
+            "categoryName": "博客站", "desc": "", "categorySiteList": [
+
+                {"name": "CSND", "url": "CSND", "icon": "URL"},
+                {"name": "博客园", "url": "CSND", "icon": "URL"},
+                {"name": "IBM中国", "url": "https://www.ibm.com/developerworks/cn/java/", "icon": ""},
+                {"name": "博客园", "url": "CSND", "icon": "URL"},
+                {"name": "博客园", "url": "CSND", "icon": "URL"},
+                {"name": "博客园", "url": "CSND", "icon": "URL"}
+
+
+            ]
+        },
+
+        {
+            "categoryName": "接私活", "desc": "", "categorySiteList": [
+
+                {"name": "CSND", "url": "https://www.cnblogs.com/", "icon": "URL"},
+                {"name": "CSND", "url": "http://www.csdn.net/", "icon": "URL"},
+                {"name": "CSND", "url": "http://www.jianshu.com/", "icon": "URL"}
+
+            ]
+        },
+
+
+        {
+            "categoryName": "IT资讯", "desc": "", "categorySiteList": [
+
+                {"name": "CSND", "url": "http://36kr.com/", "icon": "URL"},
+                {"name": "CSND", "url": "https://www.huxiu.com/", "icon": "URL"},
+                {"name": "CSND", "url": "http://www.cnetnews.com.cn/", "icon": "URL"}
+
+
+            ]
+        },
+
+
+        {
+            "categoryName": "看新闻", "desc": "", "categorySiteList": [
+
+                {"name": "网易新闻", "url": "http://news.163.com/", "icon": "URL"},
+                {"name": "凤凰资讯", "url": "http://news.ifeng.com/", "icon": "URL"},
+                {"name": "人民网", "url": "http://www.people.com.cn/", "icon": "URL"},
+                {"name": "南方都市报", "url": "http://www.nandu.com/", "icon": "URL"},
+                {"name": "美国CNN", "url": "http://www.cnn.com/", "icon": "URL"},
+                {"name": "联合国新闻", "url": "http://www.un.org/chinese/News/", "icon": "URL"},
+                {"name": "环球军事", "url": "http://mil.huanqiu.com/", "icon": "URL"},
+                {"name": "凤凰网军事", "url": "http://news.ifeng.com/mil/", "icon": "URL"},
+                {"name": "国防部", "url": "http://www.mod.gov.cn/", "icon": "URL"}
+
+            ]
+        },
+
+        {
+            "categoryName": "休闲一刻", "desc": "", "categorySiteList": [
+
+                {"name": "19楼", "url": "http://www.19lou.com/", "icon": "URL"},
+                {"name": "糗事百科", "url": "http://www.qiushibaike.com/", "icon": "URL"},
+                {"name": "暴走漫画", "url": "http://baozoumanhua.com/", "icon": "URL"}
+
+            ]
+        },
+
+        {
+            "categoryName": "热爱美图", "desc": "", "categorySiteList": [
+
+                {"name": "妹子美图", "url": "http://www.meizitu.com/", "icon": ""},
+                {"name": "美女高清", "url": "http://www.qiushibaike.com/", "icon": "URL"},
+                {"name": "美女私房照", "url": "http://www.mnsfz.com/", "icon": "URL"},
+                {"name": "4493美图", "url": "http://www.4493.com/", "icon": "URL"},
+                {"name": "美女图片", "url": "http://www.169bb.com/", "icon": "URL"}
+
+            ]
+        },
+
+
+    ]
 
 }
