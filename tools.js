96c969396c126e78f4d0c6dd7814da5b5f01c2ff
longquan <longquan@dian.so>
2019-06-23 12:35:23 +0800

    加新网站。

--- tools.js
+++ tools.js
@@ -3,80 +3,25 @@
         {
             "categoryName": "博客站", "desc": "", "categorySiteList": [
 
-                {"name": "CSND", "url": "http://www.csdn.net/", "icon": "http://file.9rmb.club/static_file/icon/csdn_favicon.ico"},
-                {"name": "博客园", "url": "https://www.cnblogs.com/", "icon": "http://file.9rmb.club/static_file/icon/cnblog_favicon.ico"},
-                {"name": "IBM中国", "url": "https://www.ibm.com/developerworks/cn/java/", "icon": ""},
-                {"name": "51CTO", "url": "http://www.51cto.com/", "icon": ""},
+                {"name": "sublime","desc": "文本编辑", "page": "http://mac.orsoon.com/Mac/160734.html", "downloadUrl": "https://mac2.orsoon.com/mac/201805/SublimeText.dmg"},
+                {"name": "iterm","desc": "命令本", "page": "http://mac.orsoon.com/Mac/163432.html", "downloadUrl": "https://mac2.orsoon.com/mac/201807/iTerm_zyc.dmg"},
+                {"name": "office","desc": "office", "page": "http://mac.orsoon.com/Mac/83311.html", "downloadUrl": "https://mac2.orsoon.com//2017/06/soft/Office2016MacGJY9UD.dmg"},
+                {"name": "edraw max","desc": "画各种的软件", "page": "http://mac.orsoon.com/Mac/164615.html", "downloadUrl": "https://mac2.orsoon.com/mac/201809/12379b107855c44eb3e16de7ffec7069.dmg"},
+                {"name": "sketch","desc": "专业矢量绘图工具", "page": "http://mac.orsoon.com/Mac/160681.html", "downloadUrl": "https://mac2.orsoon.com/mac/201805/Sketch.dmg"},
+                {"name": "postman","desc": "API管理开发工具", "page": "http://mac.orsoon.com/Mac/163454.html", "downloadUrl": "http://mdown.orsoon.com:5898/mac/201807/Postman_zyc.dmg"},
+                {"name": "Navicat Premium","desc": "db manager", "page": "http://mac.orsoon.com/Mac/163318.html", "downloadUrl": "https://mac2.orsoon.com/mac/201807/Navicat_Premium_12TYY.dmg"},
+                {"name": "Parallels Desktop","desc": "超好用虚拟机", "page": "http://mac.orsoon.com/Mac/165229.html", "downloadUrl": "https://mac2.orsoon.com/mac/201810/15f31a29fc46d77dc236f19dbfc34471.dmg"},
+                {"name": "omniplan","desc": "omniplan", "page": "http://mac.orsoon.com/Mac/167155.html", "downloadUrl": "https://mac2.orsoon.com/mac/201901/2753759a45381a8f0217fd7668ae4483.dmg"},
+                {"name": "charles","desc": "代理、抓包工具", "page": "http://mac.orsoon.com/Mac/83166.html", "downloadUrl": "https://mac2.orsoon.com/mac/201810/89b097b7bea009901621fc04320cc1bf.dmg"},
+                {"name": "Photoshop cc 2019","desc": "ps", "page": "http://mac.orsoon.com/Mac/165591.html", "downloadUrl": "http://mdown.orsoon.com:5898/mac/201810/f287b473b24825ce62aad3e5cc489671.dmg"}
 
 
-            ]
-        },
-
-        {
-            "categoryName": "接私活", "desc": "", "categorySiteList": [
-
-                {"name": "开源中国众包平台", "url": "https://zb.oschina.net/", "icon": ""},
-                {"name": "我要外包网", "url": "http://www.51waibao.net", "icon": ""},
-                {"name": "程序员的经纪人", "url": "https://www.proginn.com/", "icon": ""},
-                {"name": "快码众包", "url": "https://www.proginn.com/", "icon": ""},
-                {"name": "Coding 码 市", "url": "", "icon": ""},
-                {"name": "我爱方案网", "url": "http://www.52solution.com/kb", "icon": ""},
-                {"name": "人人开发", "url": "http://www.rrkf.com/", "icon": ""},
-                {"name": "开发邦", "url": "https://www.kaifabang.com/", "icon": ""}
-
-            ]
-        },
 
 
-        {
-            "categoryName": "IT资讯", "desc": "", "categorySiteList": [
-
-                {"name": "CSND", "url": "http://36kr.com/", "icon": ""},
-                {"name": "360Kr", "url": "https://www.huxiu.com/", "icon": ""},
-                {"name": "凤凰网", "url": "http://www.cnetnews.com.cn/", "icon": ""}
-
-
-            ]
-        },
-
-
-        {
-            "categoryName": "看新闻", "desc": "", "categorySiteList": [
-
-                {"name": "网易新闻", "url": "http://news.163.com/", "icon": ""},
-                {"name": "凤凰资讯", "url": "http://news.ifeng.com/", "icon": ""},
-                {"name": "人民网", "url": "http://www.people.com.cn/", "icon": ""},
-                {"name": "南方都市报", "url": "http://www.nandu.com/", "icon": ""},
-                {"name": "美国CNN", "url": "http://www.cnn.com/", "icon": ""},
-                {"name": "联合国新闻", "url": "http://www.un.org/chinese/News/", "icon": ""},
-                {"name": "环球军事", "url": "http://mil.huanqiu.com/", "icon": ""},
-                {"name": "凤凰网军事", "url": "http://news.ifeng.com/mil/", "icon": ""},
-                {"name": "国防部", "url": "http://www.mod.gov.cn/", "icon": ""}
-
-            ]
-        },
-
-        {
-            "categoryName": "休闲一刻", "desc": "", "categorySiteList": [
-
-                {"name": "19楼", "url": "http://www.19lou.com/", "icon": ""},
-                {"name": "糗事百科", "url": "http://www.qiushibaike.com/", "icon": ""},
-                {"name": "暴走漫画", "url": "http://baozoumanhua.com/", "icon": ""}
-
-            ]
-        },
-
-        {
-            "categoryName": "热爱美图", "desc": "", "categorySiteList": [
-
-                {"name": "妹子美图", "url": "http://www.meizitu.com/", "icon": ""},
-                {"name": "美女高清", "url": "http://www.qiushibaike.com/", "icon": ""},
-                {"name": "美女私房照", "url": "http://www.mnsfz.com/", "icon": ""},
-                {"name": "4493美图", "url": "http://www.4493.com/", "icon": ""},
-                {"name": "美女图片", "url": "http://www.169bb.com/", "icon": ""}
 
             ]
         }
+
 
 
     ]
